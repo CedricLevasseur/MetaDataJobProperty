b56e672cc0e1eed65933170ad2952e01351887c0
Cédric Levasseur <cedric.levasseur@cegedim.com>
2015-07-23 09:03:22 +0200

    no need for setter

--- src/main/java/com/cedriclevasseur/jenkinsci/plugins/metadataproject/MetaDataProject/MetadataprojectJobProperty.java
+++ src/main/java/com/cedriclevasseur/jenkinsci/plugins/metadataproject/MetaDataProject/MetadataprojectJobProperty.java
@@ -98,16 +98,8 @@ public class MetadataprojectJobProperty extends JobProperty<AbstractProject<?, ?
             return key1;
         }
 
-        public void setKey1(String key1) {
-            this.key1 = key1;
-        }
-
         public String getValue1() {
             return value1;
-        }
-
-        public void setValue1(String value1) {
-            this.value1 = value1;
         }
     }
 
